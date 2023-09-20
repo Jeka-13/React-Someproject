a51b72eab8469c33435cda55265865d10586ddc5
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-09-20 16:13:41 +0300

    Add helper function classNames

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,10 @@
+type Mods = Record<string, string | boolean>
+export function classNames (className: string, mods: Mods, additional: string[]): string {
+    return [
+        className,
+        ...Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className]) => className),
+            ...additional
+    ] .join(' ')
+}
