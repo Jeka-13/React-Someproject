752d9a160307badb932912b7b2b3551e74eab54f
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-09-21 03:17:11 +0300

    Add svg loader, theme for switcher and Button ui kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {useTheme} from "app/providers/ThemeProvider";
 
 
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
-
+    const {theme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <button onClick={toggleTheme}>TOGGLE Theme</button>
             <AppRouter/>
         </div>
     );
