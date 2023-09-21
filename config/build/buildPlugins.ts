266f592a0931c42b0b2709f131002ed075b8dd5f
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-09-22 02:26:26 +0300

    Add extract plugin, add Babel

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export function buildPlugins({path, isDev}: BuildOptions): webpack.WebpackPlugin
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
