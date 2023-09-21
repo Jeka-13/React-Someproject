82220bef9850860d72fb2d81dd82b0545fca16d6
Eugeniu Chetrar <chetrar.eugeniu@isa.utm.md>
2023-09-22 01:06:27 +0300

    Add i18n library and made chunks for different pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
@@ -13,11 +13,13 @@ const App = () => {
     const {theme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
